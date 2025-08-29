d12d13e0b532705cbf981c38dc28a995b6579243
Harshit Kr. kushwaha <harshit999kushwaha@gmail.com>
2025-08-29 11:25:40 +0530

    date-29/08/2025

--- src/MyException.java
+++ src/MyException.java
@@ -1,29 +1,35 @@
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class MyException {
-    public static void main(String[] args) {
-        int x,y,z=0;
-        Scanner sc=new Scanner(System.in);
-        try{
-            x=sc.nextInt();
-            y=sc.nextInt();
-            z=x/y;
+    public static void main(String[] args) throws IOException {
+        int x, y, z = 0;
+        DataInputStream dis = new DataInputStream(System.in);
+        Scanner sc = new Scanner(System.in);
+//        try {
+            x = dis.readInt();
+            y = dis.readInt();
+//            x=sc.nextInt();
+//            y=sc.nextInt();
+            z = x / y;
             System.out.println("Exception not allowed");
-        }
-        catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
-//        catch(ArithmeticException e){
-//            System.out.println(e.getMessage());
-//
 //        }
+//        catch(Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//        catch(ArithmeticException e){
+//           System.out.println(e.getMessage());
+//
+//       }
 //        catch(InputMismatchException e){
 //            System.out.println(e.getMessage());
+//      }
+//        finally{
+//            System.out.println("result is "+z);
 //        }
-        finally{
-            System.out.println("result is "+z);
-        }
-
+//
+//    }
     }
 }
