1a25a16f99ee10814442bab242df69607fb0b56d
Harshit Kr. kushwaha <harshit999kushwaha@gmail.com>
2025-08-29 11:25:48 +0530

    date-29/08/2025

---
+++ src/UserDefineException.java
@@ -0,0 +1,10 @@
+public class UserDefineException {
+    public void validate(int age) throws InvalidAgeException {
+        if (age<18) {
+            throw new InvalidAgeException("Cannot caste vote,Age less than 18");
+        }
+        else{
+            System.out.println("Eligible for Vote");
+        }
+    }
+}
