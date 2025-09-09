19d58ea504e9e23f02a7d23cbc110853a796b742
Harshit Kr. kushwaha <harshit999kushwaha@gmail.com>
2025-09-09 13:38:08 +0530

    09 Sept, 2025

---
+++ src/String1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class String1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+//        System.out.println(str1.substring(0, 4));
+//        System.out.println(str1.length());
+//        System.out.println(str1.toUpperCase());
+        if (str1.compareTo(str2)>0) {
+            System.out.println(str1.compareTo(str2)+"if");
+
+        } else {
+            System.out.println(str2.compareTo(str1)+"else");
+        }
+    }
+}
