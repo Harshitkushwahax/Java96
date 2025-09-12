814b0c3c5342290ca5962cc142e678e2587969da
Harshit Kr. kushwaha <harshit999kushwaha@gmail.com>
2025-09-12 12:13:22 +0530

    12 Sept, 2025

---
+++ src/Unit3/TreeSet1.java
@@ -0,0 +1,13 @@
+package Unit3;
+
+import java.util.TreeSet;
+
+public class TreeSet1 {
+    public static void main(String[] args) {
+        TreeSet<String> ts= new TreeSet<>();
+        ts.add("1");
+        ts.add("3");
+        ts.add("2");
+        System.out.println(ts);
+    }
+}
